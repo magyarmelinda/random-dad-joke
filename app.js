d7807c06a9b26afd4fe12f7bc3a0f73c6e12e027
メリンダ <m4gy4r.m3l1nd4@gmail.com>
2024-03-09 05:37:24 +0000

    refactor: follow conventions

--- app.js
+++ app.js
@@ -2,26 +2,25 @@ const button = document.querySelector('button');
 const ul = document.querySelector('#jokes');
 
 const getDadJokes = async () => {
-    try {
-        const config = {
-            headers: {
-                Accept: 'application/json'
-            }
-        }
-    
-        const res = await axios.get('https://icanhazdadjoke.com/', config);
-        return res.data.joke;
-    } catch(e) {
-        return "NO MORE JOKES FOR TODAY!";
+  try {
+    const config = {
+      headers: {
+        Accept: 'application/json'
+      }
     }
-    
+  
+    const res = await axios.get('https://icanhazdadjoke.com/', config);
+    return res.data.joke;
+  } catch(e) {
+      return "NO MORE JOKES FOR TODAY!";
+  }
 }
 
 const addNewJoke = async () => {
-    const joke = await getDadJokes();
-    const newLi = document.createElement('li');
-    newLi.innerText = joke;
-    ul.append(newLi);
+  const joke = await getDadJokes();
+  const newLi = document.createElement('li');
+  newLi.innerText = joke;
+  ul.append(newLi);
 }
 
 button.addEventListener('click', addNewJoke);
